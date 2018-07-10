3e338b0af166e85f71b7c965edc19b81c54a283a
lee <liminwanqing@users.noreply.github.com>
2018-07-10 15:33:31 +0800

    Update leetcode_03.c

--- OptiCode/leetcode_03.c
+++ OptiCode/leetcode_03.c
@@ -7,16 +7,17 @@
 */
 
 int lengthOfLongestSubstring(char* s) {
-     int len=0;
-    char *end=s,*temp;
+	int len=0;
+	char *end=s,*temp;
 	char* addressTable[256]={NULL};
 	while(*end){
 		temp = addressTable[*end];
 		addressTable[*end]=end;
 		if(temp>=s){
-		len=end-s>len?end-s:len;
-		s = temp+1;
-		}end++;
+			len=end-s>len?end-s:len;
+			s = temp+1;
+		}
+		end++;
 	}
 	len=end-s>len?end-s:len;
 	return len;
